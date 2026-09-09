f8e374fc5b916cebab4c66cbeb0149a02a74848c
Saito-N01 <300181354+Saito-N01@users.noreply.github.com>
2026-09-09 17:47:08 +0900

    feat: 初期データ投入 #2

--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Tag;
 
 class TagSeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class TagSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $tags = [
+            '質問',
+            '要望',
+            '不具合報告',
+            'ご意見',
+            'その他',
+        ];
+
+        foreach ($tags as $name) {
+            Tag::create([
+                'name' => $name,
+            ]);
+        }
     }
 }
