64cee8817b92df2890bc12b9f4b43ba27c1f4e4c
Saito-N01 <300181354+Saito-N01@users.noreply.github.com>
2026-09-11 19:43:44 +0900

    style: Lalavel Pintの実行 #7

--- app/Services/ContactSearchService.php
+++ app/Services/ContactSearchService.php
@@ -15,21 +15,21 @@ class ContactSearchService
     public function search(array $validated): Builder
     {
         return Contact::query()
-            ->when(!empty($validated['keyword']), function (Builder $query) use ($validated) {
+            ->when(! empty($validated['keyword']), function (Builder $query) use ($validated) {
                 $keyword = $validated['keyword'];
                 $query->where(function (Builder $query) use ($keyword) {
-                    $query->whereRaw("CONCAT(last_name, first_name) LIKE ?", ["%{$keyword}%"])
-                        ->orWhereRaw("CONCAT(first_name, last_name) LIKE ?", ["%{$keyword}%"])
+                    $query->whereRaw('CONCAT(last_name, first_name) LIKE ?', ["%{$keyword}%"])
+                        ->orWhereRaw('CONCAT(first_name, last_name) LIKE ?', ["%{$keyword}%"])
                         ->orWhere('email', 'like', "%{$keyword}%");
                 });
             })
-            ->when(!empty($validated['gender']) && $validated['gender'] != 0, function (Builder $query) use ($validated) {
+            ->when(! empty($validated['gender']) && $validated['gender'] != 0, function (Builder $query) use ($validated) {
                 $query->where('gender', $validated['gender']);
             })
-            ->when(!empty($validated['category_id']), function (Builder $query) use ($validated) {
+            ->when(! empty($validated['category_id']), function (Builder $query) use ($validated) {
                 $query->where('category_id', $validated['category_id']);
             })
-            ->when(!empty($validated['date']), function (Builder $query) use ($validated) {
+            ->when(! empty($validated['date']), function (Builder $query) use ($validated) {
                 $query->whereDate('created_at', $validated['date']);
             })
             ->latest();
