a3d8c1a42f69d35442d74b0f0ebc17bcf9c531d9
Saito-N01 <300181354+Saito-N01@users.noreply.github.com>
2026-09-11 19:20:38 +0900

    feat: CSVエクスポート機能の実装 #10

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,15 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreContactRequest;
+use App\Http\Requests\ExportContactRequest;
 use Illuminate\Http\Request;
 use App\Models\Contact;
 use App\Models\Category;
 use App\Models\Tag;
+use App\Services\ContactSearchService;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ContactController extends Controller
 {
+    public function __construct(
+        private ContactSearchService $contactSearchService,
+    ) {
+    }
+
     /**
      * お問い合わせフォーム入力ページ表示
      */
@@ -79,6 +87,62 @@ class ContactController extends Controller
     public function thanks(): View
     {
         return view('contact.thanks');
+    }
+
+    /**
+     * お問い合わせ一覧をCSVエクスポート
+     */
+    public function export(ExportContactRequest $request): StreamedResponse
+    {
+        $genderLabels = [1 => '男性', 2 => '女性', 3 => 'その他'];
+
+        $contacts = $this->contactSearchService
+            ->search($request->validated())
+            ->with(['category'])
+            ->get();
+
+        $fileName = 'contacts_' . now()->format('Ymd_His') . '.csv';
+
+        $headers = [
+            'Content-Type' => 'text/csv; charset=UTF-8',
+            'Content-Disposition' => "attachment; filename=\"{$fileName}\"",
+        ];
+
+        return response()->streamDownload(function () use ($contacts, $genderLabels) {
+            echo "\xEF\xBB\xBF";
+
+            $stream = fopen('php://output', 'w');
+
+            fputcsv($stream, [
+                'ID',
+                '氏名',
+                '性別',
+                'メール',
+                '電話',
+                '住所',
+                '建物',
+                'カテゴリ',
+                '内容',
+                '作成日時',
+            ]);
+
+            foreach ($contacts as $contact) {
+                fputcsv($stream, [
+                    $contact->id,
+                    $contact->last_name . ' ' . $contact->first_name,
+                    $genderLabels[$contact->gender] ?? '',
+                    $contact->email,
+                    $contact->tel,
+                    $contact->address,
+                    $contact->building,
+                    $contact->category->content ?? '',
+                    $contact->detail,
+                    $contact->created_at->format('Y-m-d H:i:s'),
+                ]);
+            }
+
+            fclose($stream);
+        }, $fileName, $headers);
     }
 }
 
